b9887fd7adac67e9638aad7627333d2966ba7d6c
Matthew Chalmers <matt@macrovue.com.au>
2017-05-30 20:01:29 +1000

    Update some doco... Boy this util has some forks on GitHub.

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from setuptools import setup, find_packages
 
 setup(name='gitstats',
-        version='1.0',
-        description='Git Statistic Utility',
-        author='Raphael Freudiger',
-        packages = find_packages(exclude=['build','dist']),
-        package_data = {
-                # Include *.gif or *.js files in the 'reporter' package:
-                'gitstats.reporter': ['*.gif', '*.js', '*.css'],
-            },
-        entry_points={
-            'console_scripts': [
-                'gitstats = gitstats:main',
-            ],
-        }
+    version='1.0',
+    description='Git Statistic Utility',
+    author='Matt Chalmers (originally Heikki Hokkanen)',
+    packages = find_packages(exclude=['build','dist']),
+    package_data = {
+            # Include *.gif or *.js files in the 'reporter' package:
+            'gitstats.reporter': ['*.gif', '*.js', '*.css'],
+        },
+    entry_points={
+        'console_scripts': [
+            'gitstats = gitstats:main',
+        ],
+    }
 )
